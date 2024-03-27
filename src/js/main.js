f86b4df5ca761a302e898797c7de9b3638922287
Goncear Valera <valeragoncear@gmail.com>
2024-03-27 17:03:39 +0200

    update slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,30 @@
 import '../scss/style.scss';
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
 
-$(function () {
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
-    $('.top-slider__inner').slick({
+const swiper = new Swiper('.top-slider__inner', {
+    modules: [Navigation, Pagination],
+    // Тут дальше ставишь сколько слайдеров тебе надо в ряд показывать, расстояние между ними
+});
 
-    })
-})
+
+/*
+* Вот тут ссылка как подключать https://swiperjs.com/get-started,
+* тут смотри что бы его юзать, надло сначала установить npm install swiper,
+* потом подключить в файле где ты его юзаешь, вот так import Swiper from 'swiper',
+* если нужна пагинация или навигация, то их тоже надо подключить, вот так
+* import { Navigation, Pagination } from 'swiper/modules';
+* и так же тебе надо в html написать разметку для слайдера
+*  */
+
+
+// $(function () {
+//
+//     $('.top-slider__inner').slick({
+//
+//     })
+// })
